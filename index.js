74e596927475bf8e44cd51bf634d3319b537e3f2
Sean <skabe89@gmail.com>
2021-02-12 12:52:17 -0500

    commit tree 6cd758abcb944911a1d2ea85cc325e5b5d04872a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+let dodger = document.getElementById("dodger"); 
+dodger.style.backgroundColor = "#000000"; 
+dodger.style.backgroundColor = "#FF69B4"; 
+dodger.style.bottom = "100px"; 
+dodger.style.bottom = "0px"; 
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 3}px`;
+  }
+} 
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 3}px`;
+  }
+} 
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
